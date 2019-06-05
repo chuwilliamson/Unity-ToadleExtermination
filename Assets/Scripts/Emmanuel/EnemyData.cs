73e23676e3858fc90d38134dc8bdd1c1ed1bef25
emmanuel.roberson3 <emmanuel.roberson3@gmail.com>
2019-06-05 12:19:38 -0500

    Interfaces, Base Classes
    Added Attack, TakeDamage interfaces. Added Entity, PlayerData, EnemyData

---
+++ Assets/Scripts/Emmanuel/EnemyData.cs
@@ -0,0 +1,22 @@
+﻿namespace Emmanuel
+{
+    public class EnemyData : Entity
+    {
+        public override float Attack(object other)
+        {
+            return base.Attack(other);
+        }
+
+        public override float TakeDamage(float dmg)
+        {
+            return base.TakeDamage(dmg);
+        }
+
+        public EnemyData(float health, float damage) : base(health, damage)
+        {
+        }
+    }
+
+   
+}
+
