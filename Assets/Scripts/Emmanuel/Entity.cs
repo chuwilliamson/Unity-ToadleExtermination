c8694a599f8d34257674c83721833c1313932a38
emmanuel.roberson3 <emmanuel.roberson3@gmail.com>
2019-06-05 12:49:01 -0500

    Added Scene, Asset menus, name string
    Created scene, added asset menus to playerdata and enemydata. Added name to constructor argument for entity. Changed argument type for attack/takedamage to Entity

--- Assets/Scripts/Emmanuel/Entity.cs
+++ Assets/Scripts/Emmanuel/Entity.cs
@@ -5,23 +5,24 @@ namespace Emmanuel
 {
     public class Entity : ScriptableObject, IAttackable, ITakeDamageable
     {
-        public Entity(float health, float damage)
+        public Entity(string name, float health, float damage)
         {
+            Name = name;
             Health = health;
             Damage = damage;
         }
 
+        public string Name { get; set; }
         protected float Damage { get; set; }
 
         protected float Health { get; set; }
 
-
-        public virtual float Attack(object other)
+        public virtual float TakeDamage(float dmg)
         {
             throw new System.NotImplementedException();
         }
 
-        public virtual float TakeDamage(float dmg)
+        public virtual float Attack(Entity other)
         {
             throw new System.NotImplementedException();
         }
--- Assets/Scripts/Emmanuel/IAttackable.cs
+++ Assets/Scripts/Emmanuel/IAttackable.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Emmanuel;
 
 namespace Assets.Scripts.Emmanuel
 {
     interface IAttackable
     {
-        float Attack(Object other);
+        float Attack(Entity other);
     }
 }
