40a535a5bb61808c16643cbbd3c07757f57ec94c
emmanuel.roberson3 <emmanuel.roberson3@gmail.com>
2019-06-05 15:46:23 -0500

    Currency, Attack and Defend
    Added currency to PlayerData, Define attack/takedamage in Entity

--- Assets/Scripts/Emmanuel/PlayerData.cs
+++ Assets/Scripts/Emmanuel/PlayerData.cs
@@ -5,18 +5,43 @@ namespace Emmanuel
     [CreateAssetMenu(menuName = "Data/Player")]
     public class PlayerData : Entity
     {
+        private int _currency = 0;
+
+        public int Currency
+        {
+            get { return _currency; }
+            set { _currency = value; }
+        }
+        
         public override float Attack(Entity other)
         {
-            return base.Attack(other);
+            return other.TakeDamage(Damage);
         }
 
-        public override float TakeDamage(float dmg)
+        public override float TakeDamage(float dmgTaken)
         {
-            return base.TakeDamage(dmg);
+            Health -= dmgTaken;
+            return Damage;
         }
 
+        public int GainCurrency(int amountGained)
+        {
+            Currency += amountGained;
+            return amountGained;
+        }
+
+        public int SpendCurrency(int amountSpent)
+        {
+            amountSpent = (amountSpent <= Currency) ? amountSpent : 0;
+            Currency -= amountSpent;
+            return amountSpent;
+        }
+        
         public PlayerData(string name, float health, float damage) : base(name, health, damage)
         {
+            Name = name;
+            Health = health;
+            Damage = damage;
         }
     }
 }
