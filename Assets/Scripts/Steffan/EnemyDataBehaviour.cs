d4de33a4724436e98e4c30fec64259083d2e5871
Brandon-Liles <50927774+Brandon-Liles@users.noreply.github.com>
2019-06-10 16:51:15 -0500

    Merge branch 'master' into brandon

---
+++ Assets/Scripts/Steffan/EnemyDataBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDataBehaviour : MonoBehaviour
+{
+	public Emmanuel.EnemyData ed;
+	// Use this for initialization
+	void Start ()
+	{
+		ed = Instantiate(ed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
