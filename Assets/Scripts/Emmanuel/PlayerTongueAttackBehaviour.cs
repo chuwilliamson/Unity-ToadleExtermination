b1923cdcf2ac677d1e4d31c0754331b9896adc50
emmanuel.roberson3 <emmanuel.roberson3@gmail.com>
2019-06-11 15:47:14 -0500

    Moved Resources, Added Behaviours
    Made an Emmanuel Folder in the Resources folder, then moved my scriptable objects there. Added ColorChangerBehaviour, and PlayerTongueAttackBehaviour script.

---
+++ Assets/Scripts/Emmanuel/PlayerTongueAttackBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	
+	//ToDo:: Make the tongue lerp to the mouse position. If its past the radius, make it lerp to a poin on the radius.
+	public AnimationCurve ac;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
