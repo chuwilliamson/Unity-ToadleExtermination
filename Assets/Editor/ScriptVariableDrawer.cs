16de98f0729792da1930615737f496243b0a9c98
Steffan Mouton <smouton8993@gmail.com>
2019-06-26 18:33:22 -0500

    @chuwilliamson discussed and demoed his GameEvent scripts

---
+++ Assets/Editor/ScriptVariableDrawer.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using Chuwilliamson.Attributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace Chuwilliamson.Attributes
+{
+
+
+
+    [CustomPropertyDrawer(typeof(ScriptVariableAttribute))]
+    public class ScriptVariableDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var height = base.GetPropertyHeight(property, label);
+            var extra = GUI.skin.button.CalcSize(label).y + EditorGUIUtility.standardVerticalSpacing;
+            return ScriptVariableAttribute.Verbose ? height + extra : height;// if it's verbose give it extra height
+        }
+
+        // Draw the property inside the given rect
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var vars = ScriptVariableAttribute.Vars.Where(t => t.GetType().IsAssignableFrom(fieldInfo.FieldType)).ToList();
+            var selected = vars.FindIndex(stat => property.objectReferenceValue == stat);
+            var contents = vars.Select(n => new GUIContent(n.name)).ToArray();
+
+            var bottomContent = new GUIContent(label.text);
+
+            if (ScriptVariableAttribute.Verbose)
+            {
+                position.height -= GUI.skin.button.CalcSize(bottomContent).y + EditorGUIUtility.standardVerticalSpacing;
+                GUI.enabled = false;
+                EditorGUI.PropertyField(position, property, label);
+                GUI.enabled = true;
+                position.y += EditorStyles.objectField.CalcSize(label).y + EditorGUIUtility.standardVerticalSpacing;
+            }
+
+            EditorGUI.BeginChangeCheck();
+
+
+            selected = EditorGUI.Popup(position, bottomContent, selected, contents);
+
+            if (!EditorGUI.EndChangeCheck()) return;
+            Debug.Log(selected.ToString());
+            Debug.Log(vars);
+            property.objectReferenceValue = vars[selected];
+            property.serializedObject.ApplyModifiedProperties();
+            property.serializedObject.Update();
+        }
+
+        public ScriptVariableAttribute ScriptVariableAttribute
+        {
+            get { return (ScriptVariableAttribute)attribute; }
+        }
+    }
+}
--- Assets/Scripts/Steffan/Behaviours/TurretBehaviour.cs
+++ Assets/Scripts/Steffan/Behaviours/TurretBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Emmanuel.ScriptableObjects;
 using UnityEngine;
 
@@ -7,17 +8,51 @@ namespace Steffan.Behaviours
 	public class TurretBehaviour : MonoBehaviour
 	{
 		public TurretData turretData;
-		private List<EnemyDataBehaviour> enemiesInRange;
-		
-		private 
-			// Use this for initialization
-			void Start () {
-		
+		private List<EnemyDataBehaviour> enemiesInRange = new List<EnemyDataBehaviour>();
+
+		private void Start()
+		{
+			turretData = Instantiate(turretData);
 		}
-	
+
 		// Update is called once per frame
 		void Update () {
 		
+		}
+
+		public void AddToEnemiesInRange(UnityEngine.Object enemy)
+		{
+			var sender = enemy as GameObject;
+			if (sender == null)
+				return;
+
+			var enemyBehaviour = sender.GetComponent<EnemyDataBehaviour>();
+			if (enemyBehaviour == null)
+				return;
+			
+			enemiesInRange.Add(enemyBehaviour);
+		}
+		public void RemoveFromEnemiesInRange(UnityEngine.Object enemy)
+		{
+			var sender = enemy as GameObject;
+			if (sender == null)
+				return;
+			
+			var enemyBehaviour = sender.GetComponent<EnemyDataBehaviour>();
+			if (enemyBehaviour == null)
+				return;
+			if (!enemiesInRange.Contains(enemyBehaviour))
+				return;
+				
+			enemiesInRange.Remove(enemyBehaviour);
+		}
+		public bool AcquireTarget()
+		{
+			return false;
+		}
+		public bool LookAtTarget()
+		{
+			return false;
 		}
 	}
 }
