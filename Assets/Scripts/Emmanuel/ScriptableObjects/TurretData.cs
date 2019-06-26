0f45eada57202f57b6858bce02489965f559b9f8
Steffan Mouton <smouton8993@gmail.com>
2019-06-26 17:20:36 -0500

    Removed damageValue from TurretData (already exists via Entity)

--- Assets/Scripts/Emmanuel/ScriptableObjects/TurretData.cs
+++ Assets/Scripts/Emmanuel/ScriptableObjects/TurretData.cs
@@ -7,8 +7,7 @@ namespace Emmanuel.ScriptableObjects
     {
         [SerializeField]
         private float cost;
-
-        public float damageValue;
+        
         public float attackRange;
         public float attackRecharge;
         public TurretData(string name, float health, float damage) : base(name, health, damage)
