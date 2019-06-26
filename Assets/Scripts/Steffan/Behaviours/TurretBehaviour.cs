4c1a26b480814df321e7f8af02f1378268754315
Steffan Mouton <smouton8993@gmail.com>
2019-06-26 17:06:18 -0500

    Added necessary fields to Emmanuel.TurretData. Created TurretDataBehaviour

--- Assets/Scripts/Steffan/Behaviours/TurretBehaviour.cs
+++ Assets/Scripts/Steffan/Behaviours/TurretBehaviour.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections.Generic;
+using Emmanuel.ScriptableObjects;
 using UnityEngine;
 
 namespace Steffan.Behaviours
 {
 	public class TurretBehaviour : MonoBehaviour
 	{
-
+		public TurretData turretData;
 		private List<EnemyDataBehaviour> enemiesInRange;
+		
 		private 
 			// Use this for initialization
 			void Start () {
