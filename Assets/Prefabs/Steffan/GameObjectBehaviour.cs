9ed5a720defc419f5607d3cf92eacb252a1e13d1
matthew.williamson <matthew.williamson@LAF-8E4F94>
2019-07-03 18:58:51 -0500

    integrate the navmesh for reference, all objects scaled by 15

---
+++ Assets/Prefabs/Steffan/GameObjectBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GameObjectBehaviour : MonoBehaviour {
+    public void SetDestination(Transform trans)
+    {
+        GetComponent<NavMeshAgent>().SetDestination(trans.position);
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public UnityEngine.Events.UnityEvent Response;
+	// Update is called once per frame
+	void Update () {
+        Response.Invoke();
+	}
+}
