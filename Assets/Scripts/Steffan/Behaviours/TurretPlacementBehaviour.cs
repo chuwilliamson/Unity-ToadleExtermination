1e5a8da85d03e5fbf84d4b0cfe01c6fe1bce4a04
Steffan Mouton <smouton8993@gmail.com>
2019-07-15 10:54:20 -0500

    Created TurretPlacementBehaviour

---
+++ Assets/Scripts/Steffan/Behaviours/TurretPlacementBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Emmanuel.Interfaces;
+using UnityEngine;
+
+public class TurretPlacementBehaviour : MonoBehaviour
+{
+	private IWheelObject _w;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void PlaceTurret()
+	{
+		var turret = 
+	}
+}
