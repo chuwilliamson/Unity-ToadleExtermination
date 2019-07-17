65abe7a4684e893d67084f10aa859b7a5fdf4dbe
Steffan Mouton <smouton8993@gmail.com>
2019-07-17 17:24:16 -0500

    Added a single function to Debug current gamestate

--- Assets/Scripts/Matthew/Global.cs
+++ Assets/Scripts/Matthew/Global.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 [CreateAssetMenu(menuName = "Global")]
-public class Global : ScriptableObject {
-
+public class Global : ScriptableObject
+{
     public void Print(string value)
     {
         Debug.Log(value);
+    }
+    
+    public void PrintState(string value)
+    {
+        Debug.Log(WaveStateStatic.IsWavePhase.ToString());
     }
 }
