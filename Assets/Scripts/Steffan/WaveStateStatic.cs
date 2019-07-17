fa998a96e9810c87834017a78550019cea72b75a
Steffan Mouton <smouton8993@gmail.com>
2019-07-17 17:24:34 -0500

    Created a Static class for keeping track of current state

---
+++ Assets/Scripts/Steffan/WaveStateStatic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class WaveStateStatic
+{
+	private static bool isWavePhase;
+	public static bool IsWavePhase
+	{
+		get { return isWavePhase; }
+	}
+
+	public static void ChangePhase()
+	{
+		isWavePhase = !isWavePhase;
+	}
+	
+}
