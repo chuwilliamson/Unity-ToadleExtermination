341961abaaf91907b65fdd162b8b9e2e8b4bfd5b
Steffan Mouton <smouton8993@gmail.com>
2019-07-19 10:23:24 -0500

    add events for GameObjectBehaviour

--- Assets/Scripts/Matthew/GameObjectBehaviour.cs
+++ Assets/Scripts/Matthew/GameObjectBehaviour.cs
@@ -1,16 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Chuwilliamson.Attributes;
 using UnityEngine;
 
-public class GameObjectBehaviour : MonoBehaviour {
+namespace Matthew
+{
+    //Raise events for all unity callback events
+    public class GameObjectBehaviour : MonoBehaviour
+    {
+        [ScriptVariable(Verbose = true)] [SerializeField]
+        private GameEvent _awakeEvent;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        [ScriptVariable(Verbose = true)] [SerializeField]
+        private GameEvent _onDestroyEvent;
+
+        [ScriptVariable(Verbose = true)] [SerializeField]
+        private GameEvent _onDisableEvent;
+
+        [ScriptVariable(Verbose = true)] [SerializeField]
+        private GameEvent _onEnableEvent;
+
+        [ScriptVariable(Verbose = true)] [SerializeField]
+        private GameEvent _startEvent;
+
+        [ScriptVariable(Verbose = true)] [SerializeField]
+        private GameEvent _updateEvent;
+
+        private void Awake()
+        {
+            _awakeEvent.Raise(gameObject);
+        }
+
+
+        private void Start()
+        {
+            _startEvent.Raise(gameObject);
+        }
+
+        private void OnEnable()
+        {
+            _onEnableEvent.Raise(gameObject);
+        }
+
+        private void OnDisable()
+        {
+            _onDisableEvent.Raise(gameObject);
+        }
+
+
+        private void Update()
+        {
+            _updateEvent.Raise(gameObject);
+        }
+
+        private void OnDestroy()
+        {
+            _onDestroyEvent.Raise(gameObject);
+        }
+    }
 }
