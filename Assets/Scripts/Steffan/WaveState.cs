7a44bc0c8b1eba1168fe95bdef30c2e1ef38fd8f
Steffan Mouton <smouton8993@gmail.com>
2019-07-19 11:43:32 -0500

    Renamed WaveStateStatic to WaveState. Renamed isWavePhase to isBuildPhase for clarity.

---
+++ Assets/Scripts/Steffan/WaveState.cs
@@ -0,0 +1,17 @@
+﻿namespace Steffan
+{
+	public static class WaveState
+	{
+		private static bool isBuildPhase;
+		public static bool IsBuildPhase
+		{
+			get { return isBuildPhase; }
+		}
+
+		public static void ChangePhase()
+		{
+			isBuildPhase = !isBuildPhase;
+		}
+	
+	}
+}
