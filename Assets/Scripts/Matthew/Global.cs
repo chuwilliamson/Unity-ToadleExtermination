9f39d023b83dccf4138fd10f3138fa27a2a81561
Steffan Mouton <smouton8993@gmail.com>
2019-07-19 09:51:33 -0500

    add loadscene method for loading scenes through the global object

--- Assets/Scripts/Matthew/Global.cs
+++ Assets/Scripts/Matthew/Global.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 [CreateAssetMenu(menuName = "Global")]
 public class Global : ScriptableObject
 {
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index, LoadSceneMode.Additive);
+    }
     public void Print(string value)
     {
         Debug.Log(value);
     }
-    
+
     public void PrintState(string value)
     {
         Debug.Log(WaveStateStatic.IsWavePhase.ToString());
