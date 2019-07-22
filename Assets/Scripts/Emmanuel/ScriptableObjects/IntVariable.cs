6ecbc2d9174239cc44c24c7cf265e853528eab25
emmanuel.roberson3 <emmanuel.roberson3@gmail.com>
2019-07-22 11:11:09 -0500

    Renamed 'IntegerVar" to IntVariable to unity could assign the script to the scriptable object

---
+++ Assets/Scripts/Emmanuel/ScriptableObjects/IntVariable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Emmanuel.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "Variables/int")]
+    public class IntVariable : MyScriptableObject
+    {
+        [SerializeField] private int value;
+
+        public int Value { get { return value; } set { this.value = value; } }
+
+        public void Modify(int val)
+        {
+            Value += val;
+        }
+    }
+}
